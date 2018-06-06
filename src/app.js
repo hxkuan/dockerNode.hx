1cda49e31d3d2f256807b79020c1b5a56ee45bbf
wali <386200854@qq.com>
2018-06-06 14:56:31 +0800

    add ico , static

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@
  * Created by wali on 2018/6/1.
  */
 import Koa from "koa";
-
+import favicon from "./middleware/favicon"
+import serve from 'koa-static'
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa, hxk';
-});
+app.use(favicon(_ROOR_ + '/favicon.ico'));
+app.use(serve(_ROOR_ + '/public'));
+
+// app.use(ctx => {
+//   ctx.body = 'Hello Koa, hxk';
+// });
 
 app.listen(3000);
